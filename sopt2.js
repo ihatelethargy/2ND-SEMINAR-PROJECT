5201c67c4520eee134bd85e2d77df4da544ae485
ihatelethargy <naturalflavor1007@naver.com>
2020-10-17 17:04:59 +0900

    getId

--- sopt2.js
+++ sopt2.js
@@ -1,5 +1,33 @@
+const API_URL = "https://api.github.com/users/";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+
+const createUsercard = (user) => {
+    console.log("user",user);
+    const cardHTML  = `
+    <h2>${user.name}</h2>
+    `;
+    main.innerHTML = cardHTML;
+}
+
+
 const getUser = async (username) =>{
-    const response = await fetch("https:https://api.github.com/user")
+    console.log(user);
+    const response = await fetch(API_URL + username)
+    const responseData = await response.json();
+   
+    createUserCard(responseData);
 };
 
-getUser();
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const user = input.value;
+
+    if(user) {
+        getUser(user);
+        input.value="";
+    }
+});
